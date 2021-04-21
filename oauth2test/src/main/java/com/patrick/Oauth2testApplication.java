50869ff94809a3303624f4c5ebf4a6caf023f81e
liaosibo <774827728@qq.com>
2021-04-21 23:54:19 +0800

    springboot整合oauth2实现github单点登陆

---
+++ oauth2test/src/main/java/com/patrick/Oauth2testApplication.java
@@ -0,0 +1,13 @@
+package com.patrick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2testApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2testApplication.class, args);
+	}
+
+}
---
+++ oauth2test/src/test/java/com/patrick/Oauth2testApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2testApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
